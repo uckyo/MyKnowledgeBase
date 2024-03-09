633fe1776777ff5bb2a1fb82585b5dc9d2f6dee6
蒋文亮 <10244284+uckyo@user.noreply.gitee.com>
2024-03-10 00:33:48 +0800

    feet: update navList

--- src/components/navList.js
+++ src/components/navList.js
@@ -5,24 +5,34 @@ export const navList=[
         url: "https://cn.vuejs.org/"
     },
     {
+        title: "Vue Router",
+        description: "The official Router for Vue.js",
+        url: "https://router.vuejs.org/"
+    },
+    {
+        title: "Pinia",
+        description: "The intuitive store for Vue.js",
+        url: "https://pinia.vuejs.org/"
+    },
+    {
         title: "Nuxt",
         description: "The Intuitive Vue Framework",
         url: "https://nuxt.com/"
     },
     {
-        title: "VitePress",
-        description: "Vite & Vue Powered Static Site Generator",
-        url: "https://vitepress.dev/"
-    },
-    {
-        title: "Element Plus",
-        description: "基于 Vue 3，面向设计师和开发者的组件库",
-        url: "https://element-plus.org/zh-CN/#/zh-CN"
-    },
-    {
         title: "React",
         description: "The library for web and native user interfaces",
         url: "https://react.dev/"
+    },
+    {
+        title: "React Router",
+        description: "The official Router for React.js",
+        url: "https://reactrouter.com/en/main"
+    },
+    {
+        title: "Redux",
+        description: "A Predictable State Container for JS Apps",
+        url: "https://reactrouter.com/en/main"
     },
     {
         title: "Next",
@@ -35,8 +45,19 @@ export const navList=[
         url: "https://cn.vitejs.dev/"
     },
     {
+        title: "VitePress",
+        description: "Vite & Vue Powered Static Site Generator",
+        url: "https://vitepress.dev/"
+    },
+    {
+        title: "Element Plus",
+        description: "基于 Vue 3，面向设计师和开发者的组件库",
+        url: "https://element-plus.org/zh-CN/#/zh-CN"
+    },
+    {
         title: "Ant Design",
         description: "助力设计开发者「更灵活」地搭建出「更美」的产品",
         url: "https://ant-design.antgroup.com/index-cn"
     },
+
 ]
